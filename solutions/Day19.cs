3421d64692563d1953ac21b103fa56c020606768
Felipe Costa <felipe.costa@sap.com>
2022-11-24 19:20:20 -0300

    Day 19 - debugging for the win

---
+++ solutions/Day19.cs
@@ -0,0 +1,48 @@
+class Day19 : IDayCommand
+{
+  public string Execute()
+  {
+    var lines = new FileReader(19).Read().ToList();
+    var instructionPointerRegisterIndex = int.Parse(lines[0].Split(" ")[1]);
+    var instructionPointer = 0;
+    var registers = Enumerable.Repeat((long)0, 6).ToArray();
+    var instructions = lines.Skip(1).Select(l => {
+      var elements = l.Split(" ");
+      return (name: elements[0], a: long.Parse(elements[1]), b: long.Parse(elements[2]), c: long.Parse(elements[3]));
+    }).ToList();
+    var operations = OperationBuilder.GetOperationsLong();
+
+    // Part 01
+    while(instructionPointer >= 0 && instructionPointer < instructions.Count)
+    {
+      var instruction = instructions[instructionPointer];
+      var operation = operations[instruction.name];
+      registers[instructionPointerRegisterIndex] = instructionPointer; 
+      operation(registers, instruction.a, instruction.b, instruction.c);
+      instructionPointer = (int) registers[instructionPointerRegisterIndex];
+      instructionPointer++;
+    }
+    var part01Register0 = registers[0];
+
+    // Part 02
+    /*registers = Enumerable.Repeat((long)0, 6).ToArray();
+    registers[0] = 1;
+    instructionPointer = 0;
+    while(instructionPointer >= 0 && instructionPointer < instructions.Count)
+    {
+      var instruction = instructions[instructionPointer];
+      var operation = operations[instruction.name];
+      registers[instructionPointerRegisterIndex] = instructionPointer; 
+      var registersBefore = string.Join(",", registers);
+      operation(registers, instruction.a, instruction.b, instruction.c);
+      Console.WriteLine($"{instruction} [{registersBefore}] => [{string.Join(",", registers)}]");
+      instructionPointer = (int) registers[instructionPointerRegisterIndex];
+      instructionPointer++;
+    }
+    */
+    // Via debugging I figured out that it was a program that calculated the factors of 10551267 and sum them :/
+    var part02Register0 = new List<int>{1, 3, 9, 383, 1149, 3061, 3447, 9183, 27549, 1172363, 3517089, 10551267}.Sum();
+
+    return $"The value of register 0 in part 01 is {part01Register0} and in part 02 is {part02Register0}";
+  }
+}
--- utils/2018/OperationBuilder.cs
+++ utils/2018/OperationBuilder.cs
@@ -22,4 +22,28 @@ class OperationBuilder
       {"eqrr", (reg, a, b, c) => { reg[c] = reg[a] == reg[b] ? 1 : 0; }},
     };
   }
+
+  public static Dictionary<string, Action<long[], long, long, long>> GetOperationsLong()
+  {
+    return new Dictionary<string, Action<long[], long, long, long>>()
+    {
+      {"addr", (reg, a, b, c) => { reg[c] = reg[a] + reg[b];}},
+      {"addi", (reg, a, b, c) => { reg[c] = reg[a] + b; }},
+      {"mulr", (reg, a, b, c) => { reg[c] = reg[a] * reg[b]; }},
+      {"muli", (reg, a, b, c) => { reg[c] = reg[a] * b; }},
+      {"banr", (reg, a, b, c) => { reg[c] = reg[a] & reg[b]; }},
+      {"bani", (reg, a, b, c) => { reg[c] = reg[a] & b; }},
+      {"borr", (reg, a, b, c) => { reg[c] = reg[a] | reg[b]; }},
+      {"bori", (reg, a, b, c) => { reg[c] = reg[a] | b; }},
+      {"setr", (reg, a, b, c) => { reg[c] = reg[a]; }},
+      {"seti", (reg, a, b, c) => { reg[c] = a; }},
+      {"gtir", (reg, a, b, c) => { reg[c] = a > reg[b] ? 1 : 0; }},
+      {"gtri", (reg, a, b, c) => { reg[c] = reg[a] > b ? 1 : 0; }},
+      {"gtrr", (reg, a, b, c) => { reg[c] = reg[a] > reg[b] ? 1 : 0;}},
+      {"eqir", (reg, a, b, c) => { reg[c] = a == reg[b] ? 1 : 0; }},
+      {"eqri", (reg, a, b, c) => { reg[c] = reg[a] == b ? 1 : 0; }},
+      {"eqrr", (reg, a, b, c) => { reg[c] = reg[a] == reg[b] ? 1 : 0; }},
+    };
+  }
+
 }
